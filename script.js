e129c9fb7233dc91746641bcf7357d3777eaad6b
dvitoS <vito.skender@gmail.com>
2021-09-07 19:03:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var link1 = document.querySelector('.1');
+var link1 = document.querySelector('.2');
+var link1 = document.querySelector('.3');
+var link1 = document.querySelector('.4');
+
+
+link1.addEventListener('click', first, false);
+function first(){
+    location.href = 'https://www.lonelyplanet.com/';
+}
+
+document.querySelector('.1').addEventListener('click', function() {
+    location.href = 'https://www.lonelyplanet.com/'
+}, false)
+
+
